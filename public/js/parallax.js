e2c93539ab4400c5f5b527df68ac6d23e26b6d6e
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-21 02:36:12 +0700

    move content inside parallax.js to script.js

--- public/js/parallax.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(() => {
-  let windowScroll = $(this).scrollTop();
-
-  $('.introContent').css({
-    'transform': 'translate(0px, ' + windowScroll / 4 + '%)'
-  });
-});
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,18 @@
+// Navbar toggler
 $(".navbar-toggler").click(function () {
   $("nav").toggleClass("custom-navbar-collapse");
 })
+
+// Parallax
+$(window).scroll(() => {
+  let windowScroll = $(this).scrollTop();
+
+  // Introduction Text Parallax
+  $(".introContent").css({
+    'transform': 'translate(0px, ' + windowScroll / 4 + '%)'
+  });
+
+  $(".contactButton img").css({
+    'transform': 'translate(0px, ' + windowScroll / 1.05 + '%)'
+  });
+});
