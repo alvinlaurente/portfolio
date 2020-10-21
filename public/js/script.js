fa715308e99583402ad831e5c0e6a694b9cca8c1
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-21 16:05:33 +0700

    add slick-carousel cards

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,56 @@ $(window).scroll(() => {
     'transform': 'translate(0px, ' + windowScroll / 1.05 + '%)'
   });
 });
+
+const slickSettings = {
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+    breakpoint: 568,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+    breakpoint: 343,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+    breakpoint: 0,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+}
+
+$(".skill-cards").slick(slickSettings)
