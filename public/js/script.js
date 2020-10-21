7fbaa4bb1eef0ff7f6421e91b30191f2f9513a17
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-21 17:44:46 +0700

    slick-carousel cards assets & styling

--- public/js/script.js
+++ public/js/script.js
@@ -17,52 +17,59 @@ $(window).scroll(() => {
   });
 });
 
+// Slick-carousel Cards
 const slickSettings = {
+  arrows: true,
   infinite: true,
   autoplay: true,
-  autoplaySpeed: 1500,
   mobileFirst: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplaySpeed: 4000
       }
     },
     {
       breakpoint: 992,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 4,
+        autoplaySpeed: 3000
       }
     },
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 4,
+        autoplaySpeed: 3000
       }
     },
     {
     breakpoint: 568,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplaySpeed: 2000
       }
     },
     {
     breakpoint: 343,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 2,
+        autoplaySpeed: 1500
       }
     },
     {
     breakpoint: 0,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplaySpeed: 1000
       }
     }
   ]
